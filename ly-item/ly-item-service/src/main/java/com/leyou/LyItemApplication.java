f5537d32ff448c3e9aedeeb98b4630dff1bea5cd
丁庆发 <dingqingfa@star-net.cn>
2019-05-25 10:04:22 +0800

    增加商品微服务框架

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
@@ -0,0 +1,19 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ Author     : dqf
+ * @ Date       : Create in 09:57 2019-05-25
+ * @ Modified By: xxx
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LyItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LyItemApplication.class, args);
+    }
+}
