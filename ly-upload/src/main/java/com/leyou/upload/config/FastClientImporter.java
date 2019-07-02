ee2ea6206e2f4d7a27209b43eb246cbd355a4976
丁庆发 <dingqingfa@star-net.cn>
2019-07-02 21:55:13 +0800

    add Fdfs - fast distribute file system, and test

---
+++ ly-upload/src/main/java/com/leyou/upload/config/FastClientImporter.java
@@ -0,0 +1,20 @@
+package com.leyou.upload.config;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+/**
+ * @ Author     : dqf
+ * @ Date       : Create in 00:19 2019-06-24
+ * @ Modified By: xxx
+ */
+
+@Configuration
+@Import(FdfsClientConfig.class)
+// 解决jmx重复注册bean的问题
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+public class FastClientImporter {
+}
